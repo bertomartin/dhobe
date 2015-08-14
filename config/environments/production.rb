0e8a45eb933d257535892e03d722ca509b7d97df
Roberto <rmartin.martin@gmail.com>
2015-08-13 23:36:44 -0400

    updating production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
     #email settings
-  config.action_mailer.default_url_options = { :host => "http://www.dhobe.com" }
+  config.action_mailer.default_url_options = { :host => "http://www.dhobe.club" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
